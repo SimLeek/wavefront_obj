61d3ad524f93ceca58de995db50c137b0a8d2a14
Josh Miklos <josh.miklos@gmail.com>
2017-10-10 18:31:03 -0700

    refactored object file loader to have code completion

--- tests/test_obj_reader.py
+++ tests/test_obj_reader.py
@@ -1,65 +1,50 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-"""
-test_wavefront_reader
-----------------------------------
-
-Tests for `wavefront_reader` module.
-"""
 from os import path
-import pytest
+import unittest as ut
 from wavefront_reader import read_objfile
 
-filepath = path.join(path.split(__file__)[0], '..', 'examples')
 
-filenames = ['untitled.obj',
-             'untitled_with_normals.obj',
-             'untitled_with_normals_and_texcoords.obj',
-             'two_complete_meshes.obj'
-             ]
+class TestObjReader(ut.TestCase):
+    def setUp(self):
+        self.script_dir = path.dirname(__file__)
 
-fnames = [path.join(filepath, name) for name in filenames]
+    def testFileExists(self):
 
-@pytest.mark.parametrize("objfile", fnames)
-def test_files_exist(objfile):
-        assert path.exists(objfile)
+        try:
+            obj_file = open(self.script_dir + "/wavefronts/untitled.obj")
+            obj_file.close()
+        except FileNotFoundError as fe:
+            self.fail("Couldn't find file. " + str(fe))
 
+    def testHasVertices(self):
+        obj_file = read_objfile(self.script_dir + "/wavefronts/untitled.obj")
+        cube = obj_file['Cube']
+        self.assertTrue(len(cube.vertexes) > 0)
 
-@pytest.mark.parametrize("objfile,expected", list(zip(fnames, [True, True, True, True])))
-def test_has_vertices(objfile, expected):
-    geoms = read_objfile(objfile)
-    cube = geoms['Cube']
-    assert (len(cube['v']) > 0) == expected
+    def test_has_normals(self):
+        geoms = read_objfile(self.script_dir + "/wavefronts/untitled_with_normals.obj")
+        cube = geoms['Cube']
+        self.assertTrue(len(cube.vertex_normals) > 0)
 
+    def test_has_no_normals(self):
+        geoms = read_objfile(self.script_dir + "/wavefronts/untitled.obj")
+        cube = geoms['Cube']
+        self.assertTrue(len(cube.vertex_normals) == 0)
 
-@pytest.mark.parametrize("objfile,expected", list(zip(fnames, [False, True, True, True])))
-def test_has_normals(objfile, expected):
-    geoms = read_objfile(objfile)
-    cube = geoms['Cube']
-    assert (len(cube['vn']) > 0) == expected
+    def test_has_texcoords(self):
+        geoms = read_objfile(self.script_dir + "/wavefronts/untitled_with_normals_and_texcoords.obj")
+        cube = geoms['Cube']
+        self.assertTrue(len(cube.vertex_textures) > 0)
 
+    def test_has_no_texcoords(self):
+        geoms = read_objfile(self.script_dir + "/wavefronts/untitled_with_normals.obj")
+        cube = geoms['Cube']
+        self.assertTrue(len(cube.vertex_textures) == 0)
 
-@pytest.mark.parametrize("objfile,expected", list(zip(fnames, [False, False, True, True])))
-def test_has_texcoords(objfile, expected):
-    geoms = read_objfile(objfile)
-    cube = geoms['Cube']
-    assert (len(cube['vt']) > 0) == expected
-
-
-@pytest.mark.parametrize("objfile", fnames)
-def test_has_texcoords(objfile):
-    geoms = read_objfile(objfile)
-    for geom in geoms.values():
-        vertrows = len(geom['v'])
-        for coord in ['vn', 'vt']:
-            if len(geom[coord]):
-                assert len(geom[coord]) == vertrows
-
-
-@pytest.mark.parametrize("objfile, count", list(zip(fnames, [1, 1, 1, 2])))
-def test_has_correct_number_of_meshes(objfile, count):
-    geoms = read_objfile(objfile)
-    assert len(geoms) == count
-
-
+    def test_has_n_meshes(self):
+        geoms = read_objfile(self.script_dir + "/wavefronts/untitled_with_normals.obj")
+        self.assertTrue(len(geoms.object_list) == 1)
+        geoms = read_objfile(self.script_dir + "/wavefronts/two_complete_meshes.obj")
+        self.assertTrue(len(geoms.object_list) == 2)
--- wavefront_reader/reading.py
+++ wavefront_reader/reading.py
@@ -1,65 +1,139 @@
 # -*- coding: utf-8 -*-
 import numpy as np
-from collections import namedtuple, defaultdict
+from collections import namedtuple, OrderedDict
 from six import iteritems
 from os import path
 
-def parse_mixed_delim_str(line):
+if False:
+    from typing import List, Dict
+
+class Face(object):
+    def __init__(self,
+                 parent_object  # type: ObjectFile
+                 ):
+        self.vertex_indices = []
+        self.vertex_texture_indices = []
+        self.vertex_normal_indices = []
+        self.parent_object = parent_object
+
+    @property
+    def vertexes(self):
+        return [self.parent_object.vertexes[v] for v in self.vertex_indices]
+
+    @property
+    def vertex_normals(self):
+        return [self.parent_object.vertex_normals[v] for v in self.vertex_normal_indices]
+
+    @property
+    def vertex_textures(self):
+        return [self.parent_object.vertex_textures[v] for v in self.vertex_texture_indices]
+
+def parse_mixed_delim_str(line, parent_obj):
     """Turns .obj face index string line into [verts, texcoords, normals] numeric tuples."""
+    face = Face(parent_obj)
     arrs = [[], [], []]
     for group in line.split(' '):
-        for col, coord in enumerate(group.split('/')):
-            if coord:
-                arrs[col].append(int(coord))
+        for type, coord in enumerate(group.split('/')):
+            if type == 0 and coord:
+                face.vertex_indices.append(int(coord))
+            elif type == 1 and coord:
+                face.vertex_texture_indices.append(int(coord))
+            elif type == 2 and coord:
+                face.vertex_normal_indices.append(int(coord))
 
-    return [tuple(arr) for arr in arrs]
+    return face
+
+class Object(object):
+    def __init__(self, name  # type: str
+                 ):
+        self.name = name
+        self.obj_props = []
+        self.vertexes = []
+        self.vertex_normals = []
+        self.vertex_textures = []
+        self.faces = []  # type: List[Face]
+        self._current_obj_prop = None
+
+        self.misc = []
+
+    def add_prop(self, prefix, value):
+        self.obj_props.append({})
+        self._current_obj_prop = self.obj_props[-1]
+        self._current_obj_prop['f'] = []
+        self._current_obj_prop[prefix] = value
+
+    def has_prop(self):
+        return self._current_obj_prop is not None
+
 
 class ObjectFile(object):
     def __init__(self):
-        pass
+        self._obj_props = OrderedDict()  # type: Dict[str, Object]
+        self.last_obj_prop = None
+
+        self.misc = OrderedDict()
+
+    def __getitem__(self,
+                    item  # type: str
+                    ): # type: (...)->Object
+        return self.object_dict[item]
+
+    def __setitem__(self,
+                    key,  # type: str
+                    value  # type: Object
+                    ):
+        self.object_dict[key] = value
+
+    @property
+    def object_list(self):
+        return [x[1] for x in self._obj_props.items()]
+
+    @property
+    def object_dict(self):
+        return self._obj_props
+
+    def add_prop(self, name):
+        self._obj_props[name] = Object(name)
+        self.last_obj_prop = self.object_list[-1]
+
+    def has_prop(self):
+        return self.last_obj_prop is not None
+
 
 def read_objfile(fname):
-    """Takes .obj filename and returns dict of object properties for each object in file."""
-    verts = defaultdict(list)
-    obj_props = []
+    """Takes .obj filename and return an ObjectFile class."""
+    obj_file = ObjectFile()
+
     with open(fname) as f:
         lines = f.read().splitlines()
 
     if 'OBJ' not in lines[0]:
         raise ValueError("File not .obj-formatted.")
 
+    # todo: assumes one object per .obj file, which is wrong
+    # todo: doesn't properly ignore comments
     for line in lines:
         if line:
             prefix, value = line.split(' ', 1)
             if prefix == 'o':
-                obj_props.append({})
-                obj = obj_props[-1]
-                obj['f'] = []
-                obj[prefix] = value
-            if obj_props:
-                if prefix[0] == 'v':
-                    verts[prefix].append([float(val) for val in value.split(' ')])
+                obj_file.add_prop(value)
+            if obj_file.has_prop():
+                if prefix == 'v':
+                    obj_file.last_obj_prop.vertexes.append([float(val) for val in value.split(' ')])
+                elif prefix == 'vn':
+                    obj_file.last_obj_prop.vertex_normals.append([float(val) for val in value.split(' ')])
+                elif prefix == 'vt':
+                    obj_file.last_obj_prop.vertex_textures.append([float(val) for val in value.split(' ')])
                 elif prefix == 'f':
-                    obj[prefix].append(parse_mixed_delim_str(value))
+                    obj_file.last_obj_prop.faces.append(parse_mixed_delim_str(value, obj_file.last_obj_prop))
                 else:
-                    obj[prefix] = value
+                    obj_file.misc[prefix] = value
 
+    return obj_file
 
-    # Reindex vertices to be in face index order, then remove face indices.
-    verts = {key: np.array(value) for key, value in iteritems(verts)}
-    for obj in obj_props:
-        obj['f'] = tuple(np.array(verts) if verts[0] else tuple() for verts in zip(*obj['f']))
-        for idx, vertname in enumerate(['v' ,'vt', 'vn']):
-            if vertname in verts:
-                obj[vertname] = verts[vertname][obj['f'][idx].flatten() - 1, :]
-            else:
-                obj[vertname] = tuple()
-        del obj['f']
-
-    geoms = {obj['o']:obj for obj in obj_props}
-
-    return geoms
-
+class MaterialFile(object):
+    def __init__(self):
+        pass
 
 def read_mtlfile(fname):
     materials = {}
